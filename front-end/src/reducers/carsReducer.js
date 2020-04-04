672d37f05a15dfb5f6d618c7e941a67f9ddab651
Damir Jazvin <damir.jazvin@hotmail.com>
2020-04-04 17:53:04 +0200

    Commit: Added carsReducer

---
+++ front-end/src/reducers/carsReducer.js
@@ -0,0 +1,42 @@
+initialState = {
+    rentacarServices: [
+        {
+            Title: "EuroCar",
+            Address: "Majora Gravrilovica 34",
+            Description: "Cheap and safe new car for your travel",
+            Pricelist: [],
+            Vehicles: [
+                {
+                    Id: 1,
+                    CarModel: "GT Mustang 56",
+                    AvailableNow: true,
+
+
+                }
+            ],
+
+            BranchOffices: [
+                {
+                    Id: 1,
+                    Title: "Dan's Car Vehicles",
+                    City: "Novi Sad",
+                    Manager: "Callahan Harry"
+                }
+            ],
+            Grades: [5, 4, 2, 1],
+            AverageGrade: 12,
+
+        }  
+    ]
+    
+}
+export default function userReducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+
+
+
