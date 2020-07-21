a27650e52a7b0d635f8a3b4c5284a15d8a6c528c
Nedeljko Zekonja <batonja@hotmail.com>
2020-07-21 19:46:26 +0200

    Fixed sintax in CarAdminHome and LineChart

--- front-end/src/components/Car/Admin/CarAdminHome.js
+++ front-end/src/components/Car/Admin/CarAdminHome.js
@@ -1,57 +1,49 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
-
 //ICONS AND AVATAR
-import Avatar from '@material-ui/core/Avatar';
-import Divider from '@material-ui/core/Divider';
+import Avatar from "@material-ui/core/Avatar";
+import Divider from "@material-ui/core/Divider";
 
-import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import InfoIcon from '@material-ui/icons/Info';
-import StarIcon from '@material-ui/icons/Star';
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
-import HomeIcon from '@material-ui/icons/Home';
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import InfoIcon from "@material-ui/icons/Info";
+import StarIcon from "@material-ui/icons/Star";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import EmojiTransportationIcon from "@material-ui/icons/EmojiTransportation";
+import HomeIcon from "@material-ui/icons/Home";
 
-
-import AdminDashboardItem from '../Utilities/AdminDashboardItem'
-import BarChart from '../Utilities/BarChart'
-import LineChart from '../Utilities/LineChart'
-
+import AdminDashboardItem from "../Utilities/AdminDashboardItem";
+import BarChart from "../Utilities/BarChart";
+import LineChart from "../Utilities/LineChart";
 
 const styles = (theme) => ({
-    adminHome: {
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignText: 'center'
-       
+  adminHome: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    alignText: "center",
+  },
+  generalProfitModal: {
+    [theme.breakpoints.down("xs", "sm", "md", "lg")]: {
+      flexDirection: "column",
+      width: "100%",
     },
-    generalProfitModal: {
-        [theme.breakpoints.down("xs", "sm", 'md', 'lg')]: {
-            flexDirection: 'column',
-            width: '100%',
-        },
-        width: '90%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        //background: '#3F51B5',
-        borderRadius: '20px',
-        margin: '10px'
-    },
-    adminHomeHeader: {
-        textAlign: 'left',
-        margin: '0 0 0 80px',
-        fontSize: '4vh'
-    },
-})
-
-const styles = (theme) => ({});
+    width: "90%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    //background: '#3F51B5',
+    borderRadius: "20px",
+    margin: "10px",
+  },
+  adminHomeHeader: {
+    textAlign: "left",
+    margin: "0 0 0 80px",
+    fontSize: "4vh",
+  },
+});
 
 const CarAdminHome = (props) => {
   const { classes } = props;
@@ -62,77 +54,73 @@ const CarAdminHome = (props) => {
       <h1>TEST</h1>
     </div>
   );
-};
 
-export default withStyles(styles)(CarAdminHome);
-    const { classes } = props
-    const profitItemInfo = {
-        infoTitle: 'Total profit',
-        info: '1000$',
-        infoDescription: 'Total profit of the car service',
-    }
-    const gradeItemInfo = {
-        infoTitle: 'Average grade',
-        info: '4.7',
-        infoDescription: 'Customer feedback',
-    }
-    const rentedCarsItemInfo = {
-        infoTitle: 'Cars rented',
-        info: '3',
-        infoDescription: 'Number of rented vehicles',
-    }
-    const stockItemInfo = {
-        infoTitle: 'Cars on stock',
-        info: '8',
-        infoDescription: 'Number of garaged wehicles',
-    }
+  const profitItemInfo = {
+    infoTitle: "Total profit",
+    info: "1000$",
+    infoDescription: "Total profit of the car service",
+  };
+  const gradeItemInfo = {
+    infoTitle: "Average grade",
+    info: "4.7",
+    infoDescription: "Customer feedback",
+  };
+  const rentedCarsItemInfo = {
+    infoTitle: "Cars rented",
+    info: "3",
+    infoDescription: "Number of rented vehicles",
+  };
+  const stockItemInfo = {
+    infoTitle: "Cars on stock",
+    info: "8",
+    infoDescription: "Number of garaged wehicles",
+  };
 
-    return (
-        <div>
-            <div className={classes.adminHomeHeader}>
-                <p><strong>Dashboard</strong> Admin Control panel</p>
-            </div>
-            <div className={classes.adminHome}>
-                <BarChart chartTitle={'Yearly profit report'} />
-                <div className={classes.generalProfitModal}>
-                    <AdminDashboardItem
-                        MainInfoAvatar={<AttachMoneyIcon fontSize='large' />}
-                        DescriptionAvatar={<InfoIcon fontSize='small' />}
-                        itemInfo={profitItemInfo}
-                    />
-                    <AdminDashboardItem
-                        MainInfoAvatar={<StarIcon fontSize='large' />}
-                        DescriptionAvatar={<InfoIcon fontSize='small' />}
-                        itemInfo={gradeItemInfo}
-                    />
-                    <AdminDashboardItem
-                        MainInfoAvatar={<EmojiTransportationIcon fontSize='large' />}
-                        DescriptionAvatar={<InfoIcon fontSize='small' />}
-                        itemInfo={rentedCarsItemInfo}
-                    />
-                    <AdminDashboardItem
-                        MainInfoAvatar={<HomeIcon fontSize='large' />}
-                        DescriptionAvatar={<InfoIcon fontSize='small' />}
-                        itemInfo={stockItemInfo}
-                    />
-                </div>
-                <LineChart
-                    chartTitle={'Rented Cars weekly report'}
-                    sygnalType={"weekly"}
-                />
-                <LineChart
-                    chartTitle={'Rented Cars monthly report'}
-                    sygnalType={"monthly"}
-                />
-                <LineChart
-                    chartTitle={'Rented Cars yearly report'}
-                    sygnalType={"yearly"}
-                />
-
-
-            </div>
+  return (
+    <div>
+      <div className={classes.adminHomeHeader}>
+        <p>
+          <strong>Dashboard</strong> Admin Control panel
+        </p>
+      </div>
+      <div className={classes.adminHome}>
+        <BarChart chartTitle={"Yearly profit report"} />
+        <div className={classes.generalProfitModal}>
+          <AdminDashboardItem
+            MainInfoAvatar={<AttachMoneyIcon fontSize="large" />}
+            DescriptionAvatar={<InfoIcon fontSize="small" />}
+            itemInfo={profitItemInfo}
+          />
+          <AdminDashboardItem
+            MainInfoAvatar={<StarIcon fontSize="large" />}
+            DescriptionAvatar={<InfoIcon fontSize="small" />}
+            itemInfo={gradeItemInfo}
+          />
+          <AdminDashboardItem
+            MainInfoAvatar={<EmojiTransportationIcon fontSize="large" />}
+            DescriptionAvatar={<InfoIcon fontSize="small" />}
+            itemInfo={rentedCarsItemInfo}
+          />
+          <AdminDashboardItem
+            MainInfoAvatar={<HomeIcon fontSize="large" />}
+            DescriptionAvatar={<InfoIcon fontSize="small" />}
+            itemInfo={stockItemInfo}
+          />
         </div>
-    )
-}
-
-export default withStyles(styles)(CarAdminHome)
+        <LineChart
+          chartTitle={"Rented Cars weekly report"}
+          sygnalType={"weekly"}
+        />
+        <LineChart
+          chartTitle={"Rented Cars monthly report"}
+          sygnalType={"monthly"}
+        />
+        <LineChart
+          chartTitle={"Rented Cars yearly report"}
+          sygnalType={"yearly"}
+        />
+      </div>
+    </div>
+  );
+};
+export default withStyles(styles)(CarAdminHome);
--- front-end/src/components/Car/Utilities/LineChart.js
+++ front-end/src/components/Car/Utilities/LineChart.js
@@ -1,148 +1,181 @@
-import React, { PureComponent } from 'react'
-import ReactEcharts from 'echarts-for-react';
-import { withStyles } from '@material-ui/core/styles'
-import moment from 'moment'
+import React, { PureComponent } from "react";
+import ReactEcharts from "echarts-for-react";
+import { withStyles } from "@material-ui/core/styles";
+import moment from "moment";
 
 const styles = (theme) => ({
-
-    lineChart: {
-        [theme.breakpoints.down("xs", "sm", 'md')]: {
-            width: '90%'
-        },
-        width: '100%',
-        height: '400px'
+  lineChart: {
+    [theme.breakpoints.down("xs", "sm", "md")]: {
+      width: "90%",
     },
-    lineChartContainer: {
-        display: 'flex',
-        //alignItems: 'center',
-        justifyContent: 'center',
-        [theme.breakpoints.down("xs", "sm", 'md')]: {
-            width: '90%'
-        },
-        width: '90%',
-        height: '400px',
-        background: '#27293D',
-        borderRadius: '10px',
-        margin: '20px 0 0 0',
-    }
-})
+    width: "100%",
+    height: "400px",
+  },
+  lineChartContainer: {
+    display: "flex",
+    //alignItems: 'center',
+    justifyContent: "center",
+    [theme.breakpoints.down("xs", "sm", "md")]: {
+      width: "90%",
+    },
+    width: "90%",
+    height: "400px",
+    background: "#27293D",
+    borderRadius: "10px",
+    margin: "20px 0 0 0",
+  },
+});
 
 class LineChart extends PureComponent {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        var state = {
-            option: this.getOption()
+    var state = {
+      option: this.getOption(),
+    };
+    this.state = state;
+  }
+  componentDidMount() {}
+
+  getOption = () => {
+    const { chartTitle } = this.props;
+    const { sygnalType } = this.props;
+    var data = [];
+    //TEST DATA
+    var dataWeekly = [120, 200, 150, 80, 70, 110, 130];
+    var dataMonthly = [
+      120,
+      200,
+      150,
+      80,
+      70,
+      110,
+      130,
+      15,
+      90,
+      21,
+      325,
+      260,
+      120,
+      200,
+      150,
+      80,
+      70,
+      110,
+      130,
+      15,
+      90,
+      21,
+      325,
+      260,
+      120,
+      200,
+      150,
+      80,
+      70,
+      110,
+      350,
+    ];
+    var dataYearly = [120, 200, 150, 80, 70, 110, 130, 15, 90, 21, 325, 260];
+
+    var date = new Date();
+    var echarts = require("echarts");
+
+    switch (sygnalType) {
+      case "weekly":
+        for (var i = 6, j = 0; i >= 0; i--, j++) {
+          let temp = moment().subtract(i, "days").format("dddd").toString();
+          data.push([temp, dataWeekly[j]]);
         }
-        this.state = state;
-    }
-    componentDidMount() {
-
-    }
-
-    getOption = () => {
-        const { chartTitle } = this.props
-        const { sygnalType } = this.props
-        var data = []
-        //TEST DATA
-        var dataWeekly = [120, 200, 150, 80, 70, 110, 130]
-        var dataMonthly = [120, 200, 150, 80, 70, 110, 130, 15, 90, 21, 325, 260, 120, 200, 150, 80, 70, 110, 130, 15, 90, 21, 325, 260, 120, 200, 150, 80, 70, 110, 350]
-        var dataYearly = [120, 200, 150, 80, 70, 110, 130, 15, 90, 21, 325, 260]
-
-        var date = new Date();
-        var echarts = require("echarts");
-
-        switch (sygnalType) {
-            case 'weekly':
-                for (var i = 6, j = 0; i >= 0; i--, j++) {
-                    let temp = moment().subtract(i, 'days').format('dddd').toString()
-                    data.push([temp, dataWeekly[j]])
-                }
-                break
-            case 'monthly':
-                for (var i = 30, j = 0; i >= 0; i--, j++) {
-                    let temp = moment().subtract(i, 'days').format('MMMM Do').toString()
-                    data.push([temp, dataMonthly[j]])
-                }
-                break
-            case 'yearly':
-                for (var i = 11, j = 0; i >= 0; i--, j++) {
-                    let temp = moment().subtract(i, 'month').format('MMM').toString()
-                    data.push([temp, dataYearly[j]])
-                }
-                break
-            default:
-                console.log("unexisting signal")
-                return {}
+        break;
+      case "monthly":
+        for (var i = 30, j = 0; i >= 0; i--, j++) {
+          let temp = moment().subtract(i, "days").format("MMMM Do").toString();
+          data.push([temp, dataMonthly[j]]);
         }
-
-        return {
-            title: {
-                text: chartTitle,
-                textStyle: {
-                    fontFamily: 'lato',
-                    color: 'white',
-                    fontSize: 25
-                },
-                padding: 20
-
-            },
-            xAxis: {
-                type: 'category',
-                axisLabel: {
-                    color: 'white',
-                    formatter: (function (value, index) {
-                        return value;
-                    })
-                }
-            },
-            yAxis: {
-                type: 'value',
-                axisLabel: {
-                    color: 'white'
-                },
-                splitLine: {
-                    show: false
-                }
-            },
-            series: [{
-                data: data,
-                type: 'line',
-                areaStyle: {
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                        offset: 0,
-                        color: '#ff0d00'
-                    }, {
-                        offset: 1,
-                        color: '#ffaa00'
-                    }])
-                },
-            }],
-
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'cross',
-                    animation: false,
-                    label: {
-                        backgroundColor: '#505765'
-                    }
-                }
-            },
+        break;
+      case "yearly":
+        for (var i = 11, j = 0; i >= 0; i--, j++) {
+          let temp = moment().subtract(i, "month").format("MMM").toString();
+          data.push([temp, dataYearly[j]]);
         }
+        break;
+      default:
+        console.log("unexisting signal");
+        return {};
     }
 
-    render() {
-        const { classes } = this.props
-        return (
-            <div className={classes.lineChartContainer}>
-                <ReactEcharts
-                    option={this.state.option}
-                    className={classes.lineChart}
-                    style={{ height: '400px' }}
-                />
-            </div>
-        )
-    }
+    return {
+      title: {
+        text: chartTitle,
+        textStyle: {
+          fontFamily: "lato",
+          color: "white",
+          fontSize: 25,
+        },
+        padding: 20,
+      },
+      xAxis: {
+        type: "category",
+        axisLabel: {
+          color: "white",
+          formatter: function (value, index) {
+            return value;
+          },
+        },
+      },
+      yAxis: {
+        type: "value",
+        axisLabel: {
+          color: "white",
+        },
+        splitLine: {
+          show: false,
+        },
+      },
+      series: [
+        {
+          data: data,
+          type: "line",
+          areaStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: "#ff0d00",
+              },
+              {
+                offset: 1,
+                color: "#ffaa00",
+              },
+            ]),
+          },
+        },
+      ],
+
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "cross",
+          animation: false,
+          label: {
+            backgroundColor: "#505765",
+          },
+        },
+      },
+    };
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.lineChartContainer}>
+        <ReactEcharts
+          option={this.state.option}
+          className={classes.lineChart}
+          style={{ height: "400px" }}
+        />
+      </div>
+    );
+  }
 }
-export default withStyles(styles)(LineChart)
+export default withStyles(styles)(LineChart);
